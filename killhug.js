e2b90ce184c03863e3a940beb4b7a070c03352d1
Álvaro Torralba <donfrutosgomez@gmail.com>
2019-11-20 17:09:53 -0300

    Add files via upload
    Add source code (killhug.js).

---
+++ killhug.js
@@ -0,0 +1,48 @@
+/*
+ * killhug.js
+ * 
+ * Copyright 2019 Alvarito050506 <donfrutosgomez@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+function sleep(ms)
+{
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function kill()
+{
+	while (true)
+	{
+		var pid = "0";
+		Object.keys(world.room.players).forEach((id) => {
+			if(world.room.players[id]["critterId"] == "huggable")
+			{
+				pid = id;
+			}
+		});
+		socket.emit("attack", pid);
+		socket.emit("click", {
+			x: world.room.players[getPlayerId()].x + (Math.ceil(Math.random() * 100) < 50 ? Math.ceil(Math.random() * 50) : 0 - Math.ceil(Math.random() * 50)),
+			y: world.room.players[getPlayerId()].y + (Math.ceil(Math.random() * 100) < 50 ? Math.ceil(Math.random() * 50) : 0 - Math.ceil(Math.random() * 50))
+		});
+		await sleep(1000);
+	}
+}
+
+kill();
